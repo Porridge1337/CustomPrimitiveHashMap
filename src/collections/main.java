ffeac4de8b27b074ad74e5c9534009cd59dfb99a
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2022-09-21 19:04:50 +0300

    Проверка реализации

--- src/collections/main.java
+++ src/collections/main.java
@@ -4,21 +4,26 @@ import collections.map.CustomHashMap;
 import collections.map.CustomMap;
 import dto.Student;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class main {
 
     public static void main(String[] args) {
+
+        Map<String, String> tree = new TreeMap<>();
 
         CustomMap<Integer, Student> map = new CustomHashMap<>();
         map.put(1, new Student(1, "Raz", "Dva"));
         map.put(2, new Student(2, "Raz", "Dva"));
         map.put(3, new Student(2, "Raz1", "Dva1"));
         map.put(6, new Student(2, "Raz1", "Dva1"));
-        map.put(2, new Student(2, "Raz1", "Dva1"));
-        map.put(2, new Student(2, "Raz1", "Dva1"));
+        map.put(5, new Student(2, "Raz1", "Dva1"));
+        map.put(4, new Student(2, "Raz1", "Dva1"));
 
-        System.out.println(map.keySet());
+        //System.out.println(map.keys());
         System.out.println(map.get(1));
-        System.out.println(map.remove(2));
+        System.out.println(map.remove(null));
         System.out.println(map.size());
 
         CustomMap<String, Student> map2 = new CustomHashMap<>();
@@ -29,7 +34,7 @@ public class main {
         map2.put("aghfdf", new Student(2, "Raz1", "Dva1"));
         map2.put("ghgggffgh", new Student(2, "Raz1", "Dva1"));
 
-        System.out.println(map2.keySet());
+        System.out.println(map2.keys());
         System.out.println(map2.size());
 
         map.removeAll();
